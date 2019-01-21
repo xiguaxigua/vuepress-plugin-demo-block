2ffefb1e016bfc2f24b479346f26589f35a8bce3
Melon <guang.xing@liulishuo.com>
2019-01-21 16:22:18 +0800

    deploy docs

---
+++ bt.js
@@ -0,0 +1,7 @@
+window._hmt = window._hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?72e1ef2463dc80e5a1d91214ddec2ba9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
